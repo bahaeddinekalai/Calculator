312d3b33b5d085495e67e68d0bbc3f18f1cd7a46
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-04-09 06:45:47 +0100

    feat(calc): Add operate function to perform math operations
    Create a new function called operate that takes an operator
        and two numbers as input parameters and calls the
        appropriate math operation function based on the operator.

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNumber = 0;
+let secondeNumber = 0;
+let operator = "+";
+
+function operate(operator, firstNumber, secondeNumber) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondeNumber);
+      break;
+    case "-":
+      subtract(firstNumber, secondeNumber);
+      break;
+    case "*":
+      multiply(firstNumber, secondeNumber);
+      break;
+    case "/":
+      divide(firstNumber, secondeNumber);
+      break;
+    default:
+      add(firstNumber, secondeNumber);
+      break;
+  }
+}
