dbd6b7734ced44700f014de5f42f3855e538ff8e
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-04-10 05:12:25 +0100

    feat(UI): Create HTML and CSS for calculator
    Add HTML and CSS to display calculator with buttons for digits,
    basic math functions, equal, delete and clear.

--- index.js
+++ index.js
@@ -37,3 +37,23 @@ function operate(operator, firstNumber, secondeNumber) {
       break;
   }
 }
+
+let grid = document.querySelector(".main-buttons");
+const size = 4;
+for (let i = 0; i < size; i++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  for (let j = 0; j < size; j++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    row.appendChild(column);
+  }
+  grid.appendChild(row);
+}
+let cells = document.querySelectorAll(".column");
+
+const content = [7, 4, 1, ".", 8, 5, 2, 0, 9, 6, 3, "=", "÷", "×", "-", "+"];
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].innerHTML = `${content[i]}`;
+}
